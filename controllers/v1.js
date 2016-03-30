aa5ed50c35c00dd9ca1c0a93ef8aa378b20a0425
Tyler Blair <hidendra@griefcraft.com>
2016-03-29 22:45:47 -0300

    Update MBR url

--- controllers/v1.js
+++ controllers/v1.js
@@ -4,7 +4,7 @@ let Promise = require('promise');
 let request = require('request');
 let Quote = require('../models/quote.js');
 
-let MBR_BASE_URL = 'http://134.190.188.171:3000/api';
+let MBR_BASE_URL = 'http://54.175.15.171:3000/api';
 let MUN_BASE_URL = 'http://127.0.0.1:3000/v1';
 
 /**
