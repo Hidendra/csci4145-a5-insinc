80d4d7e788ef6d84c8a40477a6bb4c17b5442b2b
Tyler Blair <hidendra@griefcraft.com>
2016-03-30 12:45:10 -0300

    Send 404 instead of 500 with invalid mortID

--- controllers/v1.js
+++ controllers/v1.js
@@ -167,6 +167,6 @@ module.exports.postQuote = (req, res) => {
             });
         })
         .catch((error) => {
-            res.sendStatus(500);
+            res.sendStatus(404);
         });
 };
